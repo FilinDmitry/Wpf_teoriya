b499c3922d185b335dd13117cc0fc424fbf44876
FilinDmitry <filin.dmitry07@gmail.com>
2025-12-03 13:31:04 +0300

    completed

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -25,29 +26,119 @@ namespace WpfApp1
             InitializeComponent();
             
         }
-
-        private void RandomButton_Click(object sender, RoutedEventArgs e)
+        double finalValue;
+        private void Thumb_OnDragCompleted(object sender, DragCompletedEventArgs e)
         {
-            Random random = new Random();
-            int a, b;
+            Slider slider = (Slider)sender;
+            finalValue = slider.Value;
 
-            if (!int.TryParse(MinRandTextBox.Text, out a) || !int.TryParse(MaxRandTextBox.Text, out b))
-            {
-                MessageBox.Show("Неправильный ввод", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-                MinRandTextBox.Text = ""; // Обращаемся к элементу MinRandTextBox и меняем его Text на пустой
-                MaxRandTextBox.Text = ""; // Обращаемся к элементу MaxRandTextBox и меняем его Text на пустой
-                return;
-            }
-
-            if (a > b)
-            {
-                int x = a;
-                a = b;
-                b = x;
-            }
-
-            // Обращаемся к элементу OutputTextBlock и меняем его Text на результат случайной генерации числа
-            OutputTextBlock.Text = random.Next(a, b).ToString();
+            Shtuk.Text = $"{finalValue} шт";
+            summa();
         }
+
+        private int oplata;
+        private void ToggleButton_OnChecked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = (RadioButton)sender; // явное преобразование object в RadioButton
+            switch (rb.Content.ToString())
+            {
+                case "Наличные":
+                    oplata = 1;
+                    break;
+                case "Карта":
+                    oplata = 2;
+                    break;
+                case "Онлайн":
+                    oplata = 3;
+                    break;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Оплата {oplata} телефон = {phone} имя = {name} коичество = {finalValue} сумма = {Sun.Text}"); 
+        }
+        string phone;
+        private void TextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.Text[0]);
+            
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox s = sender as TextBox;
+            phone = s.Text;
+        }
+        string name;
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            TextBox s = sender as TextBox;
+            name = s.Text;
+        }
+
+       
+        bool dop_1 = false;
+        bool dop_2 = false;
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (dop_1)
+            { dop_1 = false; }
+            else { dop_1 = true; }
+            summa();
+        }
+
+        private void CheckBox_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (dop_2)
+            { dop_2 = false; }
+            else { dop_2 = true; }
+            summa();
+        }
+        private void summa()
+        {
+            double sum = 0;
+            sum += 500 * finalValue;
+            if (dop_1)
+            { sum += 100; }
+            if (dop_2) { sum += 200; };
+            Sun.Text = sum.ToString();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (Shtuk != null)
+            {
+                Slider slider = (Slider)sender;
+                finalValue = slider.Value;
+
+                Shtuk.Text = $"{finalValue} шт";
+                summa();
+            }
+        }
+        /*
+private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+{
+TextBox tb = sender as TextBox;
+string text = tb.Text; // получаем текст из TextBox
+
+// Получаем информацию об изменениях
+foreach (var change in e.Changes)
+{
+// change.Offset — позиция изменения в тексте
+// change.AddedLength — количество добавленных символов
+// change.RemovedLength — количество удалённых символов
+MessageBox.Show($"Изменение в позиции {change.Offset}");
+}
+}
+
+private void ComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+{
+ComboBox cb = sender as ComboBox;
+ComboBoxItem item = cb.SelectedItem as ComboBoxItem;
+// item.Content — выбранный элемент
+// cb.SelectedIndex — индекс выбора
+MessageBox.Show($"Выбран {item.Content.ToString()}.");
+}*/
     }
 }
