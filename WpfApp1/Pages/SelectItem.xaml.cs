34f293bfcba43c014a1f1e0bbea3b66d59311595
FilinDmitry <filin.dmitry07@gmail.com>
2026-03-04 11:33:33 +0300

    Подключенны службы

--- WpfApp1/Pages/SelectItem.xaml.cs
+++ WpfApp1/Pages/SelectItem.xaml.cs
@@ -13,20 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace WpfApp1.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для SelectItem.xaml
     /// </summary>
-    
-    public partial class MainWindow : Window
+    public partial class SelectItem : Page
     {
-        public MainWindow()
+        public SelectItem()
         {
-            
             InitializeComponent();
-            
         }
-
     }
 }
