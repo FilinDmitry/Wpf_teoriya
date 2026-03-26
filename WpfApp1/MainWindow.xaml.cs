ecbfebc3f9703df88b788c37447d6e9a0ce3b23d
FilinDmitry <filin.dmitry07@gmail.com>
2026-03-26 14:19:16 +0300

    созданы странички

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -23,34 +23,19 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
-            Cal.BlackoutDates.Add(new CalendarDateRange(new DateTime(2026, 03, 27)));
-            Cal.BlackoutDates.Add(new CalendarDateRange(DateTime.MinValue, DateTime.Today.AddDays(-1)));
         }
 
-        
-        
-
-        
-
-
-        private void Window_KeyDown(object sender, KeyEventArgs e)
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
         {
-            TB.Text = $"Нажата клавиша {e.Key}";
-        }
-
-        private void Window_KeyUp(object sender, KeyEventArgs e)
-        {
-            TB.Text = $"Отпущена клавиша {e.Key}";
-        }
-
-        private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
-        {
-            Calendar calendar = sender as Calendar;
-            DateTime dateTime = (DateTime)calendar.SelectedDate;
-            Year.Text = dateTime.Year.ToString();
-            Month.Text = dateTime.Month.ToString();
-            DayOfWeek.Text = dateTime.DayOfWeek.ToString();
-            Date.Text = dateTime.Date.ToString("dd.MM.yyyy");
+            NavigationService navigation = NavigationService.GetNavigationService(MainFrame);
+            if (navigation != null)
+            {
+                if (navigation.CanGoBack)
+                {
+                    navigation.GoBack();
+                }
+                
+            }
         }
     }
 }
