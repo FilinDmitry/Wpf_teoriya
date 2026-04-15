8a2f9584642cbce75942cfe5c7c17f3cd0df38a3
FilinDmitry <filin.dmitry07@gmail.com>
2026-04-15 18:39:09 +0300

    Менеджер товары

--- WpfApp1/Pages/MenegerPages/ManagerTovarPage.xaml.cs
+++ WpfApp1/Pages/MenegerPages/ManagerTovarPage.xaml.cs
@@ -20,9 +20,64 @@ namespace WpfApp1.Pages.MenegerPages
     /// </summary>
     public partial class ManagerTovarPage : Page
     {
+        List<Product> products;
         public ManagerTovarPage()
         {
             InitializeComponent();
+            products = Core.Context.Product.ToList();
+            ListBox.ItemsSource = products;
+        }
+
+        private void TovarFreeze_Click(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = sender as CheckBox;
+
+            Product prod = checkBox.DataContext as Product;
+            prod.IsFreeze = (bool)checkBox.IsChecked;
+            Core.Context.SaveChanges();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tBox = sender as TextBox;
+            int d;
+            if (!int.TryParse(tBox.Text, out d))
+            {
+                MessageBox.Show("Введите корректное значение скидки");
+                return;
+            }
+            if (d >= 100)
+            {
+                MessageBox.Show("Введите значение скидки <100");
+                return;
+            }
+            Product prod = tBox.DataContext as Product;
+            prod.Discount = d;
+            Core.Context.SaveChanges();
+            
+
+        }
+
+        private void DiscoundFreeze_Click(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = sender as CheckBox;
+
+            Product prod = checkBox.DataContext as Product;
+            prod.IsFreezeDiscount = (bool)checkBox.IsChecked;
+            Core.Context.SaveChanges();
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            TextBox tBox = sender as TextBox;
+            Product prod = tBox.DataContext as Product;
+            prod.Description = tBox.Text;
+            Core.Context.SaveChanges();
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.Text[0]);
         }
     }
 }
